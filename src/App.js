47e8fbbd93c991ad11860e680f0d15884940d60c
jorge garcia <jorge.garcia.celis@gmail.com>
2024-04-29 17:41:29 -0600

    avance

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './image/freecodecamp-logo.png';
+import Tarea from './components/Tarea';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='logo-container'>
+        <img 
+          src={logo}
+          alt=''
+          className='logo'
+          />
+      </div>
+      <div className='tareas-app'>
+        <h1>Mis Tareas</h1>
+      </div>
     </div>
   );
 }
